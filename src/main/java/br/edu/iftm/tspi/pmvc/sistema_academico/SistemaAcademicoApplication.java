9caa5163d07a0460b9b73ccbc4bdec30d31f5550
Augusto <augustocesarrezende64@gmail.com>
2025-02-17 12:58:08 -0300

    chave retirada

--- src/main/java/br/edu/iftm/tspi/pmvc/sistema_academico/SistemaAcademicoApplication.java
+++ src/main/java/br/edu/iftm/tspi/pmvc/sistema_academico/SistemaAcademicoApplication.java
@@ -2,6 +2,10 @@ package br.edu.iftm.tspi.pmvc.sistema_academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SistemaAcademicoApplication {
@@ -10,4 +14,19 @@ public class SistemaAcademicoApplication {
 		SpringApplication.run(SistemaAcademicoApplication.class, args);
 	}
 
+	@Configuration
+	public static class CorsConfiguracao {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
